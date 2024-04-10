eb953802e7c7c26b772da97211a20665ce50ab3e
dvs-codes <132570673+dvs-codes@users.noreply.github.com>
2024-04-10 09:03:39 +0530

    add linkedList functionality that checks for keymatch.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,24 @@ class HashMap{
 
   set(key, value) {
     let index = this.hash(key)
-    this.table[index] = new Node(value)
+    //if table is not empty we check for content
+    if (this.table[index]) {
+      let currentNode = this.table[index]
+      // go deeper if keys dont match and also if nextNode is not null
+      while (Object.keys(currentNode)[0]!==key && currentNode.nextNode!==null) {
+        currentNode = currentNode.nextNode
+      }
+      //if keys match, update value
+      if (Object.keys(currentNode)[0]===key) {
+        currentNode[key] = value
+      } 
+      //else we are at end of linkedList, add new node here
+      else {
+        currentNode.nextNode = new Node(key, value)
+      }  
+    } else {
+      this.table[index] = new Node(key, value)
+    }
   }
 
   get(key) {
@@ -58,18 +75,24 @@ class HashMap{
 }
 
 class Node{
-  constructor(value, nextNode=null) {
-    this.value = value
+  constructor(key,value, nextNode=null) {
+    this[key] = value
     this.nextNode = nextNode
   }
 }
 let hashmap = new HashMap()
 
 hashmap.set('carlos', "value1")
-hashmap.set('carlos', "value2")
 hashmap.set('carla', "n1")
-console.log(hashmap.get('john'))
-console.log(hashmap.get('carla'))
+hashmap.set('carla', "n2")
+hashmap.set('carla', "newesr")
+
+hashmap.set('jon', "j1")
+hashmap.set('jon', "j2")
+hashmap.set('carla', "n3")
+hashmap.set('jon', "j3")
+hashmap.set('carlos', "c3")
 
 console.log(hashmap.table)
-console.log(hashmap.length())
+
+
